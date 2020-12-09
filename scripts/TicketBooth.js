a14834fc7be4fb415554be06a721c6a413633695
sosinatolossa <sosinatolossa16@gmail.com>
2020-12-09 16:07:01 -0600

    modified modules

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -2,9 +2,13 @@ export const contentTarget = document.querySelector(".entry") //give me a refere
 export const eventHub = document.querySelector("#state-fair")
 
 eventHub.addEventListener("click", (event) => {
-    if (event.id === ("rideTicket")) {
-        const rideEvent = new CustomEvent("rideTicketPurchased")
-        eventHub.dispatchEvent(rideTicketPurchased)
+    if (event.target.id === "rideTicket") {
+        const rideEvent = new CustomEvent("rideTicketPurchased", {
+            detail: {
+                rideButton: event.target.value //when rideButton is clicked, it's gonna send that change to the one that is listening
+            }
+        })
+        eventHub.dispatchEvent(rideEvent)
     }
 })
 
--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -2,7 +2,12 @@ const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
 export const RideTicketHolders = () => {
-    eventHub.addEventListener("", customEvent => {
+    eventHub.addEventListener("rideTicketPurchased", event => {
+        contentTarget.innerHTML += `
+            <div class="person rider"></div>
+        `
     })
 }
+
+
 
