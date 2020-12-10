53bd34b0061392f5e886853da3106f8dd18365c0
sosinatolossa <sosinatolossa16@gmail.com>
2020-12-10 16:11:38 -0600

    added modules

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -12,10 +12,46 @@ eventHub.addEventListener("click", (event) => {
     }
 })
 
+eventHub.addEventListener("click", (event) => {
+    if (event.target.id === "foodTicket") {
+        const foodEvent = new CustomEvent("foodTicketPurchased", {
+            detail: {
+                foodButton: event.target.value //when food button is clicked, it's gonna send that change to the one that is listening
+            }
+        })
+        eventHub.dispatchEvent(foodEvent)
+    }
+})
+
+eventHub.addEventListener("click", (event) => {
+    if (event.target.id === "gameTicket") {
+        const gameEvent = new CustomEvent("gameTicketPurchased", {
+            detail: {
+                gameButton: event.target.value //when game button is clicked, it's gonna send that change to the one that is listening
+            }
+        })
+        eventHub.dispatchEvent(gameEvent)
+    }
+})
+
+eventHub.addEventListener("click", (event) => {
+    if (event.target.id === "sideshowTicket") {
+        const sideshowEvent = new CustomEvent("sideshowTicketPurchased", {
+            detail: {
+                sideshowButton: event.target.value //when game button is clicked, it's gonna send that change to the one that is listening
+            }
+        })
+        eventHub.dispatchEvent(sideshowEvent)
+    }
+})
+
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
             <button id="rideTicket">Ride Ticket</button>
+            <button id="foodTicket">Food Ticket</button>
+            <button id="gameTicket">Game Ticket</button>
+            <button id="sideshowTicket">Sideshow Ticket</button>
         </div>
     `
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 // Import and invoke the ticket booth component function
 import { TicketBooth } from "./TicketBooth.js"
 import { RideTicketHolders } from "./rides/RideTicketHolders.js"
+import { FoodTicketHolders } from "./food/FoodTicketHolder.js"
+import { GameTicketHolders } from "./games/GameTicketHolder.js"
+import { SideshowTicketHolders} from "./sideshows/SideshowTicketHolder.js"
 
 TicketBooth()
 RideTicketHolders()
+FoodTicketHolders()
+GameTicketHolders()
+SideshowTicketHolders()
